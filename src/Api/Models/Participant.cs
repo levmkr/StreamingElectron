8384e3fe0aa93779796f8554d56b355bf38003e1
Levon Mkrtchian <levonmk@gmail.com>
2016-04-11 20:39:35 +0200

    new api project

---
+++ src/Api/Models/Participant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class Participant
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Email { get; set; }
+    }
+}
---
+++ src/Api/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Participant> Participants { get; set; } 
+    }
+}
