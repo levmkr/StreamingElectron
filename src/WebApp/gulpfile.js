b170d3fb6cb90941f226c4513ca40e1d2d509757
Levon Mkrtchian <levonmk@gmail.com>
2016-04-24 17:38:25 +0200

    bunch of new stuff

--- src/WebApp/gulpfile.js
+++ src/WebApp/gulpfile.js
@@ -2,25 +2,30 @@
 
 "use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer');
 
-var input = './wwwroot/Static/scss/*.scss';
-//var input = './wwwroot/lib/foundation-sites/scss/*.scss';
-var output = './wwwroot/Static/css/';
+var input = './wwwroot/Static/scss/**/*.scss',
+    output = './wwwroot/Static/css/';
 
 var sassOptions = {
     errLogToConsole: true,
+    outputStyle: 'expanded'
     //outputStyle: 'compressed'
 };
+
+var processors = [autoprefixer({ browsers: ['last 3 version'] })];
 
 gulp.task('sass', function () {
     gulp.src(input)
         .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(postcss(processors))
         .pipe(gulp.dest(output));
 });
 
-//gulp.task('default', function () {
-//    gulp.watch('Static/sass/*.scss', ['sass'])
-//        .on('error', function (err) { console.log(err); this.emit('end'); });
-//});
+gulp.task('watch', function () {
+    gulp.watch(input, ['sass'])
+        .on('error', function (err) { console.log(err); this.emit('end'); });
+});
